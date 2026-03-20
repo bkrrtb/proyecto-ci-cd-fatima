8c4145ecaeff172a7d06dc51a82bdc24487da316
Bkrrtb <fatimama2bn@email.com>
2026-03-20 00:03:43 +0000

    Cambiar mensaje final de la app

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Servidor desplegado correctamente en Kubernetes!"
+    return "Hola, soy Fátima Mohammed. Despliegue automático correcto en Kubernetes!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
